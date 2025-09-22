210ee9ea4b67e2b712a6528248ee8ed985d7e9a6
Jayant Panwar <jayantpanwarofficial@gmail.com>
2025-09-22 19:24:38 +0530

    Added Q1: Arithmetic Operators and Q2: Data Types

---
+++ python-ques/arithmeticoperators.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+sums= a+b
+div= a/b
+multi= a*b
+modulus=a%b
+print("use of +",sums)
+print("use of /",div)
+print("use of *",multi)
+print("use of %",modulus)
---
+++ python-ques/datatypes.py
@@ -0,0 +1,8 @@
+a = 5
+b = "strings"
+c = 5.5
+d = True
+print(("class of integer"),type(a))
+print(("class of string"),type(b))
+print(("class of float"),type(c))
+print(("class of boolean value"),type(d))
